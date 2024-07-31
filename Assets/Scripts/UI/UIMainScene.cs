5891cb2f8b53a9f6260e646cc137d1a8940993a1
thaiiii <88318343+thaiiii@users.noreply.github.com>
2024-07-31 19:02:51 +0700

    31/7
    Implement data persistence between sessions

--- Assets/Scripts/UI/UIMainScene.cs
+++ Assets/Scripts/UI/UIMainScene.cs
@@ -29,6 +29,11 @@ public class UIMainScene : MonoBehaviour
         ResourceDB.Init();
     }
 
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
     private void OnDestroy()
     {
         Instance = null;
